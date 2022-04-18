07d372176f0c63c7f143c0b8cedcf1dca8b3b7c6
MureiTipe <43036883+murei123@users.noreply.github.com>
2022-04-18 12:11:00 +0300

    Add files via upload

---
+++ software_UART-master/Sender/Sender/main.c
@@ -0,0 +1,38 @@
+/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "uart.h"
+
+
+//==========================================================================================
+// IMPLEMENTATION
+//==========================================================================================
+
+
+void SW_UART_ReadCallback(unsigned char byte)
+{
+}
+
+
+int main(void)
+{
+    UART_Init(SW_UART_ReadCallback);
+    
+    char byteToSend;
+    char *str = "Hello from sender\r\n";
+    unsigned short ind = 0;
+
+    while (1) 
+    {
+        byteToSend = str[ind];
+        ind++;
+        if( str[ind] == '\0')
+        {
+            ind = 0;
+        }
+        
+        UART_SendByte(byteToSend);
+    }
+}
+
